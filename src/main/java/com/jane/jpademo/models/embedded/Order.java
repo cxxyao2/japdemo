1ac2b7e62a06bc722dcaf7cc43474fbedc9b4d4f
jennifer <cxxyao2@gmail.com>
2025-03-15 19:03:46 -0400

    embedded id

---
+++ src/main/java/com/jane/jpademo/models/embedded/Order.java
@@ -0,0 +1,24 @@
+package com.jane.jpademo.models.embedded;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="_order")
+public class Order {
+    @EmbeddedId
+    private OrderId id;
+
+    private String  orderInfo;
+    private String anotherField;
+
+
+}
